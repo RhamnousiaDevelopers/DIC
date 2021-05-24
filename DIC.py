3127082b5cb5e1a252e8cc3a17b1fc3b5f0b5a0d
RhamnousiaDevelopers <84054273+RhamnousiaDevelopers@users.noreply.github.com>
2021-05-24 14:07:01 -0700

    Update DIC.py

--- DIC.py
+++ DIC.py
@@ -61,76 +61,21 @@ def launch_chroot():
 def prep_fs():
     os.system("sudo mksquashfs ~/LIVE_BOOT/chroot ~/LIVE_BOOT/staging/live/filesystem.squashfs -e boot")
     os.system("sudo cp ~/LIVE_BOOT/chroot/boot/vmlinuz-* ~/LIVE_BOOT/staging/live/vmlinuz && sudo cp ~/LIVE_BOOT/chroot/boot/initrd.img-* ~/LIVE_BOOT/staging/live/initrd")   #copy kernel and initramfs to live directory
-    isolinux_cfg = """
-        cat <<\'EOF\' >$HOME/LIVE_BOOT/staging/isolinux/isolinux.cfg
-UI vesamenu.c32
+    
+    os.system("sudo cp isolinux.cfg ~/LIVE_BOOT/staging/isolinux/")
+    
+    os.system("sudo cp grub.cfg ~/LIVE_BOOT/staging/boot/grub")
 
-MENU TITLE Boot Menu
-DEFAULT linux
-TIMEOUT 600
-MENU RESOLUTION 640 480
-MENU COLOR border       30;44   #40ffffff #a0000000 std
-MENU COLOR title        1;36;44 #9033ccff #a0000000 std
-MENU COLOR sel          7;37;40 #e0ffffff #20ffffff all
-MENU COLOR unsel        37;44   #50ffffff #a0000000 std
-MENU COLOR help         37;40   #c0ffffff #a0000000 std
-MENU COLOR timeout_msg  37;40   #80ffffff #00000000 std
-MENU COLOR timeout      1;37;40 #c0ffffff #00000000 std
-MENU COLOR msg07        37;40   #90ffffff #a0000000 std
-MENU COLOR tabmsg       31;40   #30ffffff #00000000 std
-
-LABEL linux
-  MENU LABEL RhamnousiaOS [BIOS/ISOLINUX]
-  MENU DEFAULT
-  KERNEL /live/vmlinuz
-  APPEND initrd=/live/initrd boot=live
-
-LABEL linux
-  MENU LABEL RhamnousiaOS [BIOS/ISOLINUX] (nomodeset)
-  MENU DEFAULT
-  KERNEL /live/vmlinuz
-  APPEND initrd=/live/initrd boot=live nomodeset
-EOF"""
-
-    os.system(isolinux_cfg)
-    grub_cfg = """
-cat <<\'EOF\' >~/LIVE_BOOT/staging/boot/grub/grub.cfg
-search --set=root --file /DEBIAN_CUSTOM
-
-set default=\"0\"
-set timeout=30
-
-# If X has issues finding screens, experiment with/without nomodeset.
-
-menuentry \"RhamnousiaOS [EFI/GRUB]\" {
-    linux ($root)/live/vmlinuz boot=live
-    initrd ($root)/live/initrd
-}
-
-menuentry \"RhamnousiaOS [EFI/GRUB] (nomodeset)\" {
-    linux ($root)/live/vmlinuz boot=live nomodeset
-    initrd ($root)/live/initrd
-}
-EOF"""
-
-    os.system(grub_cfg)
-    grub_standalone_cfg = """
-cat <<\'EOF\' >$HOME/LIVE_BOOT/tmp/grub-standalone.cfg
-search --set=root --file /DEBIAN_CUSTOM
-set prefix=($root)/boot/grub/
-configfile /boot/grub/grub.cfg
-EOF"""
-
-    os.system(grub_standalone_cfg)
-    os.system("touch $HOME/LIVE_BOOT/staging/DEBIAN_CUSTOM")
+    os.system("sudo cp grub-standalone.cfg /LIVE_BOOT/tmp")
+    os.system("sudo touch $HOME/LIVE_BOOT/staging/DEBIAN_CUSTOM")
 
     #copy BIOS/legacy boot required files to workspace
-    os.system("cp /usr/lib/ISOLINUX/isolinux.bin \"~/LIVE_BOOT/staging/isolinux/\" && cp /usr/lib/syslinux/modules/bios/* \"~/LIVE_BOOT/staging/isolinux/\"")
+    os.system("sudo cp /usr/lib/ISOLINUX/isolinux.bin \"~/LIVE_BOOT/staging/isolinux/\" && sudo cp /usr/lib/syslinux/modules/bios/* \"~/LIVE_BOOT/staging/isolinux/\"")
     #copy EFI boot files to ws
-    os.system("cp -r /usr/lib/grub/x86_64-efi/* \"~/LIVE_BOOT/staging/boot/grub/x86_64-efi/\"")
+    os.system("sudo cp -r /usr/lib/grub/x86_64-efi/* \"~/LIVE_BOOT/staging/boot/grub/x86_64-efi/\"")
 
 def gen_efi_img():
-    os.system("grub-mkstandalone --format=x86_64-efi --output=~/LIVE_BOOT/tmp/bootx64.efi --locales=\"\" --fonts=\"\" \"boot/grub/grub.cfg=~/LIVE_BOOT/tmp/grub-standalone.cfg\"")
+    os.system("sudo grub-mkstandalone --format=x86_64-efi --output=~/LIVE_BOOT/tmp/bootx64.efi --locales=\"\" --fonts=\"\" \"boot/grub/grub.cfg=~/LIVE_BOOT/tmp/grub-standalone.cfg\"")
 
     make_cmd = """
     (cd $HOME/LIVE_BOOT/staging/EFI/boot && \
@@ -143,7 +88,7 @@ def gen_efi_img():
 
 def build_iso(iso_name):
 
-    os.system("xorriso -as mkisofs -iso-level 3 -o \"~/LIVE_BOOT/"+str(iso_name)+".iso\" -full-iso9660-filenames -volid \"OS\" -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -eltorito0boot isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table --eltorito-catalog isolinux/isolinux.cat -eltorito-alt-boot -e /EFI/boot/efiboot.img -no-emul-boot -isohybrid-gpt-basdat -append_partition 2 0xef ~/LIVE_BOOT/staging/EFI/boot/efiboot.img \"~/LIVE_BOOT/staging\"")
+    os.system("sudo xorriso -as mkisofs -iso-level 3 -o \"~/LIVE_BOOT/"+str(iso_name)+".iso\" -full-iso9660-filenames -volid \"OS\" -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -eltorito0boot isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table --eltorito-catalog isolinux/isolinux.cat -eltorito-alt-boot -e /EFI/boot/efiboot.img -no-emul-boot -isohybrid-gpt-basdat -append_partition 2 0xef ~/LIVE_BOOT/staging/EFI/boot/efiboot.img \"~/LIVE_BOOT/staging\"")
 
 
 
